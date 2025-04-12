8999f79fea740fa6ada7daa09d5f746a76160a5b
BeeHive <jtrasiti@beehive.systems>
2025-04-12 13:14:44 -0600

    update twilio.py

--- twilio/twilio.py
+++ twilio/twilio.py
@@ -70,7 +70,7 @@ class TwilioLookup(commands.Cog):
                         embed.add_field(name="Caller name", value=caller_name_info.get("caller_name", "Unknown").title(), inline=True)
                         embed.add_field(name="Caller type", value=caller_name_info.get("caller_type", "Unknown").title(), inline=True)
                         embed.add_field(name="Carrier name", value=carrier_info.get("name", "Unknown"), inline=True)
-                        embed.add_field(name="Carrier type", value=carrier_info.get("type", "Unknown"), inline=True)
+                        embed.add_field(name="Carrier type", value=carrier_info.get("type", "Unknown").upper(), inline=True)
                         embed.add_field(name="Mobile country code", value=carrier_info.get("mobile_country_code", "Unknown"), inline=True)
                         embed.add_field(name="Mobile network code", value=carrier_info.get("mobile_network_code", "Unknown"), inline=True)
                         if carrier_info.get("error_code") is not None:
