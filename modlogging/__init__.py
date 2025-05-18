fa36852966441230316404495ac7da0a3cb88423
BeeHive <jtrasiti@beehive.systems>
2025-05-18 17:40:14 -0600

    Refactor logging cog import in modlogging module. Updated setup function to instantiate ModLogging instead of Logging, aligning with recent codebase changes and improving maintainability.

--- modlogging/__init__.py
+++ modlogging/__init__.py
@@ -1,12 +1,12 @@
 import json
 from pathlib import Path
 
-from .logging import Logging
+from .modlogging import ModLogging
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot):
-    cog = Logging(bot)
+    cog = ModLogging(bot)
     await bot.add_cog(cog)
